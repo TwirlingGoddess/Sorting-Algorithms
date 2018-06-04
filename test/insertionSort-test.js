48707b3affa6ffcae2e8968cde20809ad0423a65
Lee Graham <graham.la3@gmail.com>
2018-06-03 19:53:53 -0600

    Implement Insertion sort and setup for testign

--- test/insertionSort-test.js
+++ test/insertionSort-test.js
@@ -1,10 +1,14 @@
 import {assert} from 'chai';
 import insertionSort from '../lib/insertionSort.js';
 
+const unSortedArray = [1, 5, 7, 9];
+
   describe('test', function() {
     it('should return true', function() {
       assert.equal(true, true);
     });
-  });    it('should take an array', function() {
+    it('should take an array', function() {
       assert.equal(insertionSort.array, []);
-    })
+    });
+
+})
